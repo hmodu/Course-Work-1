7315d5db8bb3cddf117715e9bf4ce36741af57a6
Modu <35815941+hmodu@users.noreply.github.com>
2021-03-18 14:53:17 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,73 @@
+let products = [
+    {
+      "id": 1001,
+      "image": "file.jpg",
+      "subject": "Physics",
+      "price": 50,
+      "location": "Geneva",
+      "spaces": 5
+    },
+    {
+      "id": 1002,
+      "subject": "Chemistry",
+      "price": 35,
+      "location": "Lagos",
+      "spaces": 5
+    },
+    {
+      "id": 1003,
+      "subject": "Biology",
+      "price": 38,
+      "location": "Abuja",
+      "spaces": 5
+    },
+    {
+      "id": 1004,
+      "subject": "Psycology",
+      "price": 39,
+      "location": "Ontario",
+      "spaces": 5
+    },
+    {
+      "id": 1005,
+      "subject": "Geography",
+      "price": 42,
+      "location": "Granada",
+      "spaces": 5
+    },
+    {
+      "id": 1006,
+      "subject": "Astrology",
+      "price": 49,
+      "location": "Conackry",
+      "spaces": 5
+    },
+    {
+      "id": 1007,
+      "subject": "Engineering",
+      "price": 38,
+      "location": "Munich",
+      "spaces": 5
+    },
+    {
+      "id": 1008,
+      "subject": "Computer",
+      "price": 41,
+      "location": "Timbuktu",
+      "spaces": 5
+    },
+    {
+      "id": 1009,
+      "subject": "Mathematics",
+      "price": 47,
+      "location": "Abuja",
+      "spaces": 5
+    },
+      {
+      "id": 1010,
+      "subject": "Sociology",
+      "price": 26,
+      "location": "Toronto",
+      "spaces": 5
+    }
+  ]
